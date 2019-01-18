86f84094620ece995c6e059653cb0737bdc48b7a
Guillaume Riou <guiloua2@hotmail.com>
2019-01-18 14:24:40 -0500

    stufffffff

--- init.el
+++ init.el
@@ -56,6 +56,8 @@
 
 (use-package company-tern)
 (add-to-list 'company-backends 'company-tern)
+(add-hook 'js2-mode-hook (lambda ()
+                           (tern-mode)))
 ;; smooth scrolling and visual fluffy stuff
 (use-package sublimity)
 ;; (require 'sublimity-scroll)
@@ -441,6 +443,8 @@ That is, a string used to represent it on the tab bar."
 
 (use-package js2-mode
   :ensure t)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
 (use-package web-mode
   :ensure t)
 (use-package monokai-theme)
@@ -685,3 +689,88 @@ Version 2017-11-01"
 
 (global-auto-revert-mode)
 (setq-default indent-tabs-mode nil)
+
+;; inlines move-lines (https://github.com/targzeta/move-lines/blob/master/move-lines.el) because why not
+
+(defun move-lines--internal (n)
+  "Moves the current line or, if region is actives, the lines surrounding
+region, of N lines. Down if N is positive, up if is negative"
+
+  (let* (text-start
+         text-end
+         (region-start (point))
+         (region-end region-start)
+         swap-point-mark
+         delete-latest-newline)
+
+    (when (region-active-p)
+      (if (> (point) (mark))
+          (setq region-start (mark))
+        (exchange-point-and-mark)
+        (setq swap-point-mark t
+              region-end (point))))
+
+    ;; text-end and region-end
+    (end-of-line)
+    ;; If point !< point-max, this buffers doesn't have the trailing newline.
+    (if (< (point) (point-max))
+        (forward-char 1)
+      (setq delete-latest-newline t)
+      (insert-char ?\n))
+    (setq text-end (point)
+          region-end (- region-end text-end))
+
+    ;; text-start and region-start
+    (goto-char region-start)
+    (beginning-of-line)
+    (setq text-start (point)
+          region-start (- region-start text-end))
+
+    ;; STEP 2: cut and paste.
+    (let ((text (delete-and-extract-region text-start text-end)))
+      (forward-line n)
+      ;; If the current-column != 0, I have moved the region at the bottom of a
+      ;; buffer doesn't have the trailing newline.
+      (when (not (= (current-column) 0))
+        (insert-char ?\n)
+        (setq delete-latest-newline t))
+      (insert text))
+
+    ;; STEP 3: Restoring.
+    (forward-char region-end)
+
+    (when delete-latest-newline
+      (save-excursion
+        (goto-char (point-max))
+        (delete-char -1)))
+
+    (when (region-active-p)
+      (setq deactivate-mark nil)
+      (set-mark (+ (point) (- region-start region-end)))
+      (if swap-point-mark
+          (exchange-point-and-mark)))))
+
+(defun move-lines-up (n)
+  "Moves the current line or, if region is actives, the lines surrounding
+region, up by N lines, or 1 line if N is nil."
+  (interactive "p")
+  (if (eq n nil)
+      (setq n 1))
+  (move-lines--internal (- n)))
+
+(defun move-lines-down (n)
+  "Moves the current line or, if region is actives, the lines surrounding
+region, down by N lines, or 1 line if N is nil."
+  (interactive "p")
+  (if (eq n nil)
+      (setq n 1))
+  (move-lines--internal n))
+
+(defun move-lines-binding ()
+  "Sets the default key binding for moving lines. M-p or M-<up> for moving up
+and M-n or M-<down> for moving down."
+  (global-set-key (kbd "M-p") 'move-lines-up)
+  (global-set-key (kbd "M-<up>") 'move-lines-up)
+  (global-set-key (kbd "M-n") 'move-lines-down)
+  (global-set-key (kbd "M-<down>") 'move-lines-down))
+(move-lines-binding)
